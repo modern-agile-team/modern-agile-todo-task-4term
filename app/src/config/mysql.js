ff7fd58db07c8d7760f4d55ba144333108eaee2c
Yonghoon <yonghoonj98@gmail.com>
2022-06-02 16:07:57 +0900

    DELETE(exam): 모든 코드 삭제 빈 파일 설정

--- app/src/config/mysql.js
+++ app/src/config/mysql.js
@@ -3,10 +3,10 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host:,
+  user:,
+  password:,
+  database:,
 });
 
 module.exports = db;
