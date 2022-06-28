931c40c6d40ba2a22284f49835b82d057947be90
JUHA <khabh@naver.com>
2022-06-28 13:35:18 +0900

    router ctroller 구현

--- app/src/apis/ToDo.ctrl.js
+++ app/src/apis/ToDo.ctrl.js
@@ -1,1 +1,58 @@
 "use strict";
+const logger = require("../../src/config/logger");
+const Todo = require("../models/ToDo");
+
+const ToDo = {
+  output: (req, res) => {
+    logger.info(`GET / 200 "초기 페이지 로드"`);
+    res.render("index");
+  },
+  create: async (req, res) => {
+    const todo = new Todo(req.body);
+    const response = await todo.addTodo();
+    log(response, {
+      method: "POST",
+      status: response.err ? 400 : 201,
+    });
+    return res.status(response.err ? 400 : 200).json(response);
+  },
+  read: async (req, res) => {
+    const todo = new Todo(req.body);
+    const response = await todo.getAllTodo();
+    return res.json(response);
+  },
+  update: async (req, res) => {
+    const todo = new Todo(req.body);
+    const response = await todo.updateTodo();
+    log(response, {
+      method: "PATCH",
+      status: response.err ? 400 : 200,
+    });
+    return res.status(response.err ? 400 : 200).json(response);
+  },
+  delete: async (req, res) => {
+    const todo = new Todo(req.body);
+    const response = await todo.removeTodo();
+    log(response, {
+      method: "DELETE",
+      status: response.err ? 400 : 200,
+    });
+    return res.status(response.err ? 400 : 200).json(response);
+  },
+};
+
+module.exports = {
+  ToDo,
+};
+
+const log = (response, url) => {
+  if (response.err) {
+    logger.error(
+      `${url.method} / ${url.status} Response: ${response.success}, ${response.err}`
+    );
+  } else {
+    logger.info(
+      `${url.method} / ${url.status} Response: ${response.success}, msg: ${response.msg}`
+    );
+  }
+};
--- app/src/apis/index.js
+++ app/src/apis/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const ctrl = require("./ToDo.ctrl");
+
+router.get("/", ctrl.ToDo.output);
+router.post("/", ctrl.ToDo.create);
+// router.get("/hi", ctrl.ToDo.read);
+router.patch("/", ctrl.ToDo.update);
+router.delete("/", ctrl.ToDo.delete);
+router.put("/", ctrl.ToDo.read);
+
+module.exports = router;
