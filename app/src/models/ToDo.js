c695da18b0e62c921073dbbe7597f4ba48cbcee2
JUHA <khabh@naver.com>
2022-06-28 13:36:42 +0900

    todos DB 연결 후 모델 구현

--- app/src/models/ToDo.js
+++ app/src/models/ToDo.js
@@ -1,1 +1,46 @@
 "use strict";
+
+const ToDoStorage = require("./ToDoStorage");
+
+class Todo {
+  constructor(body) {
+    this.body = body;
+  }
+
+  async addTodo() {
+    try {
+      const body = this.body;
+      const response = await ToDoStorage.addTodo(body);
+      return response;
+    } catch (err) {
+      return { success: false, err };
+    }
+  }
+
+  async getAllTodo() {
+    const response = await ToDoStorage.getTodos();
+    return Object.values(JSON.parse(JSON.stringify(response)));
+  }
+
+  async updateTodo() {
+    try {
+      const body = this.body;
+      const response = await ToDoStorage.updateTodo(body);
+      return response;
+    } catch (err) {
+      return { success: false, err };
+    }
+  }
+
+  async removeTodo() {
+    try {
+      const body = this.body;
+      const response = await ToDoStorage.removeTodo(body);
+      return response;
+    } catch (err) {
+      return { success: false, err };
+    }
+  }
+}
+
+module.exports = Todo;
--- app/src/models/ToDoStorage.js
+++ app/src/models/ToDoStorage.js
@@ -1,1 +1,54 @@
 "use strict";
+
+const db = require("../config/mysql");
+const fs = require("fs").promises;
+
+class ToDoStorage {
+  static getTodos() {
+    return new Promise((resolve, reject) => {
+      const query = "SELECT id,description,is_check FROM todos";
+      db.query(query, (err, data) => {
+        if (err) reject(`${err}`);
+        else resolve(data);
+      });
+    });
+  }
+
+  static addTodo(todoInfo) {
+    return new Promise((resolve, reject) => {
+      const query = "INSERT INTO todos(description,is_check) VALUES(?,?);";
+      db.query(query, [todoInfo.description, todoInfo.is_check], (err) => {
+        if (err) reject(`${err}`);
+        else resolve({ success: true, msg: "테이블에 데이터를 추가했습니다." });
+      });
+    });
+  }
+
+  static updateTodo(todoInfo) {
+    return new Promise((resolve, reject) => {
+      const query =
+        "UPDATE todos SET description = ?, is_check = ? WHERE id = ?;";
+      db.query(
+        query,
+        [todoInfo.description, todoInfo.is_check, todoInfo.id],
+        (err) => {
+          if (err) reject(`${err}`);
+          else
+            resolve({ success: true, msg: "테이블의 데이터를 변경했습니다." });
+        }
+      );
+    });
+  }
+
+  static removeTodo(todoInfo) {
+    return new Promise((resolve, reject) => {
+      const query = "DELETE FROM todos WHERE id=?";
+      db.query(query, [todoInfo.id], (err) => {
+        if (err) reject(`${err}`);
+        else resolve({ success: true, msg: "테이블의 데이터를 삭제했습니다." });
+      });
+    });
+  }
+}
+
+module.exports = ToDoStorage;
