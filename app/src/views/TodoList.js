3baf7eb74965c5d7dd16cecf75422bbbc94adc44
JUHA <khabh@naver.com>
2022-06-28 13:33:31 +0900

    프론트 CRUD 기능 구현

--- app/src/views/TodoList.js
+++ app/src/views/TodoList.js
@@ -3,97 +3,194 @@ const todoListElem = document.querySelector(".todo-list");
 const todoAddBtn = document.querySelector(".add-btn");
 
 let todos = [];
-let id = 0;
+let id = 1;
+let is_clickedRevice = [];
 
-const setTodos = (newTodos) => {
+const setClickedInfo = (todos) => {
+  is_clickedRevice = [];
+  todos.forEach((todo) => {
+    is_clickedRevice.push({ id: todo.id, is_clickedRevice: false });
+  });
+};
+
+function insertDatabase(todoInfo) {
+  fetch("/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todoInfo),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res) {
+        console.log("create 정상 실행");
+      }
+    })
+    .catch((err) => {
+      console.error("post 중 에러 발생");
+    });
+}
+function getDatabase() {
+  fetch("/", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todos),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res) {
+        todos = res;
+        setClickedInfo(todos);
+      }
+    })
+    .then(() => {
+      paintTodos();
+    })
+    .catch((err) => {
+      console.error("post 중 에러 발생");
+    });
+}
+
+function removeDatabase(todoInfo) {
+  fetch("/", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todoInfo),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res) {
+        console.log("delete 정상 실행");
+      }
+    })
+    .catch((err) => {
+      console.error("delete 중 에러 발생");
+    });
+}
+
+function updateDatabase(todoInfo) {
+  fetch("/", {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todoInfo),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res) {
+        console.log("patch 정상 실행");
+      }
+    })
+    .catch((err) => {
+      console.error("patch 중 에러 발생");
+    });
+}
+const setTodos = (newTodos, newClickedInfo) => {
   todos = newTodos;
+  is_clickedRevice = newClickedInfo;
 };
 
 const getAllTodos = () => {
-  return todos;
+  return [todos, is_clickedRevice];
 };
 
 const appendTodos = (text) => {
   const newId = id++;
-  const newTodos = [
-    ...getAllTodos(),
-    { id: newId, is_check: false, description: text, is_clikedRevice: false },
-  ];
-  setTodos(newTodos);
+  insertDatabase({ id: newId, is_check: 0, description: text });
+  getDatabase();
   paintTodos();
 };
 
 const deleteTodo = (todoId) => {
-  const newTodos = getAllTodos().filter((todo) => todo.id !== todoId);
-  setTodos(newTodos);
+  removeDatabase({ id: todoId });
+  getDatabase();
   paintTodos();
 };
 
 const completeTodo = (todoId) => {
-  const newTodos = getAllTodos().map((todo) =>
-    todo.id === todoId ? { ...todo, is_check: !todo.is_check } : todo
-  );
-  setTodos(newTodos);
+  const todo = todos.filter((todo) => todo.id === todoId);
+  todo[0].is_check = todo[0].is_check === 0 ? 1 : 0;
+  updateDatabase(todo[0]);
+  getDatabase();
   paintTodos();
 };
 
 const onClickReviceTodo = (e, todoId) => {
-  const newTodos = getAllTodos().map((todo) =>
-    todo.id === todoId
-      ? { ...todo, is_clikedRevice: !todo.is_clikedRevice }
-      : todo
+  const newClickedInfo = getAllTodos()[1].map((clickedInfo) =>
+    clickedInfo.id === todoId
+      ? { ...clickedInfo, is_clickedRevice: !clickedInfo.is_clickedRevice }
+      : clickedInfo
   );
-  setTodos(newTodos);
+  setTodos(todos, newClickedInfo);
   paintTodos();
 };
 
 const updateTodo = (text, todoId) => {
-  const newTodos = getAllTodos().map((todo) =>
-    todo.id === todoId ? { ...todo, description: text } : todo
-  );
-  setTodos(newTodos);
+  const todo = todos.filter((todo) => (todo.id = todoId));
+  todo[0].description = text;
+  updateDatabase(todo[0]);
+  getDatabase();
   paintTodos();
 };
 
 const paintTodos = () => {
   todoListElem.innerHTML = null;
-  const allTodos = getAllTodos();
+  const allTodos = getAllTodos()[0];
+  const is_clickedRevice = getAllTodos()[1];
 
   allTodos.forEach((todo) => {
     const reviceBtnElem = document.createElement("button");
     reviceBtnElem.classList.add("reviceBtn");
-    reviceBtnElem.addEventListener("click", (event) =>
-      onClickReviceTodo(event, todo.id)
-    );
+    reviceBtnElem.addEventListener("click", (event) => {
+      event.stopPropagation();
+      onClickReviceTodo(event, todo.id);
+    });
     reviceBtnElem.innerHTML = "수정";
 
     const todoItemElem = document.createElement("li");
     todoItemElem.classList.add("todo-item");
-    todoItemElem.addEventListener("click", () => completeTodo(todo.id));
+    todoItemElem.addEventListener("click", () => {
+      completeTodo(todo.id);
+    });
 
     const delBtnElem = document.createElement("button");
     delBtnElem.classList.add("delBtn");
-    delBtnElem.addEventListener("click", () => deleteTodo(todo.id));
+    delBtnElem.addEventListener("click", (event) => {
+      event.stopPropagation();
+      deleteTodo(todo.id);
+    });
     delBtnElem.innerHTML = "X";
 
-    const todoElem = document.createElement("input");
+    let todoElem = document.createElement("div");
     todoElem.classList.add("todo");
-    todoElem.value = todo.description;
-    todoElem.disabled = true;
+    todoElem.innerText = todo.description;
 
-    if (todo.is_check) {
-      todoItemElem.classList.add("checked");
-    }
-    if (todo.is_clikedRevice) {
+    if (
+      is_clickedRevice.filter((clickInfo) => clickInfo.id === todo.id)[0]
+        .is_clickedRevice
+    ) {
       reviceBtnElem.innerHTML = "완료";
+      todoElem = document.createElement("input");
+      todoElem.classList.add("todo");
       todoElem.classList.add("moddable");
-      todoElem.disabled = false;
+      todoElem.value = todo.description;
+
       todoElem.addEventListener("click", (event) => {
         event.stopImmediatePropagation();
         reviceBtnElem.addEventListener("click", (event) => {
           updateTodo(event.target.parentNode.childNodes[1].value, todo.id);
         });
       });
+    }
+
+    if (todo.is_check) {
+      todoItemElem.classList.add("checked");
     }
 
     todoItemElem.appendChild(delBtnElem);
@@ -103,19 +200,16 @@ const paintTodos = () => {
   });
 };
 
-const init = () => {
-  todoInputElem.addEventListener("keypress", (e) => {
-    if (e.key === "Enter" && e.target.value) {
-      appendTodos(e.target.value);
-      todoInputElem.value = "";
-    }
-  });
-  todoAddBtn.addEventListener("click", () => {
-    if (todoInputElem.value) {
-      appendTodos(todoInputElem.value);
-      todoInputElem.value = "";
-    }
-  });
-};
-
-init();
+getDatabase();
+todoInputElem.addEventListener("keypress", (e) => {
+  if (e.key === "Enter" && e.target.value) {
+    appendTodos(e.target.value);
+    todoInputElem.value = "";
+  }
+});
+todoAddBtn.addEventListener("click", () => {
+  if (todoInputElem.value) {
+    appendTodos(todoInputElem.value);
+    todoInputElem.value = "";
+  }
+});
