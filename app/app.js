4175de2e76b7b620d209acf7f55a36ca4aa5e409
JUHA <khabh@naver.com>
2022-06-28 13:45:49 +0900

    todolist app 설정

--- app/app.js
+++ app/app.js
@@ -1,20 +1,25 @@
 "use strict";
-
+//모듈
 const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const fs = require("fs");
+
 const app = express();
-const home = (req, res) => {
-  res.render("index");
-};
+dotenv.config();
+//라우팅
+const ToDo = require("./src/apis/index");
 
-const PORT = 3000;
-
-app.use(express.static(`${__dirname}/src`));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-app.get("/", home);
+app.use(express.static(`${__dirname}/src`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-  console.log("test");
-});
+app.get("/", ToDo);
+app.post("/", ToDo);
+app.delete("/", ToDo);
+app.patch("/", ToDo);
+app.put("/", ToDo);
 
 module.exports = app;
